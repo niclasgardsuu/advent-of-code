b5594d3733633a34ea8f6d3f290e6fffba1dc458
niclasgardsuu <niclas.gards@hotmail.se>
2023-10-25 20:47:40 +0200

    timed calculations

--- 2021/06/fish.py
+++ 2021/06/fish.py
@@ -1,5 +1,9 @@
 from functools import cache
 
+import sys
+sys.setrecursionlimit(100000)
+sys.set_int_max_str_digits(100000)
+
 @cache
 def zero_fish(days):
     if days <= 0:
@@ -11,4 +15,36 @@ with open("input.txt",'r') as f:
     data = f.read()
 fishes = [int(a) for a in data.split(',')]
 
-print(sum(map(lambda x: zero_fish(256-x),fishes)))
+import time
+
+tic = time.perf_counter()
+p1 = sum(map(lambda x: zero_fish(8-x),fishes))
+toc = time.perf_counter()
+print(f'Part 1: {toc - tic:0.6f} sec')
+
+zero_fish.cache_clear()
+
+tic = time.perf_counter()
+p2 = sum(map(lambda x: zero_fish(256-x),fishes))
+toc = time.perf_counter()
+print(f'Part 2: {toc - tic:0.6f} sec')
+
+zero_fish.cache_clear()
+
+tic = time.perf_counter()
+p1 = sum(map(lambda x: zero_fish(80-x),fishes))
+p2 = sum(map(lambda x: zero_fish(256-x),fishes))
+toc = time.perf_counter()
+print(f'Part 1 & 2: {toc - tic:0.6f} sec')
+
+zero_fish.cache_clear()
+
+# tic = time.perf_counter()
+# p3 = 0
+# for i in range(1000):
+#     p3 += sum(map(lambda x: zero_fish((i+1)*1000-x),fishes))
+# toc = time.perf_counter()
+# print(f'Part 3: {toc - tic:0.6f} sec')
+# # This does finish executing! About ~6 seconds. Pretty cool considering the original time complexity.
+
+print(p1, p2, sep='\n')
